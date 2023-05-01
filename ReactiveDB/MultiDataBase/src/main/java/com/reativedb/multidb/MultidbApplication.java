63177dfb548eb0ce217a4c95776e3650ecebc1f6
TAYLEE <89365465+TAEHYUKLEE@users.noreply.github.com>
2023-05-01 15:51:59 +0900

    change of App Name

--- ReactiveDB/MultiDataBase/src/main/java/com/reativedb/multidb/MultidbApplication.java
+++ ReactiveDB/MultiDataBase/src/main/java/com/reativedb/multidb/MultidbApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MultiDataBaseApplication {
+public class MultidbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MultiDataBaseApplication.class, args);
+		SpringApplication.run(MultidbApplication.class, args);
 	}
 
 }
